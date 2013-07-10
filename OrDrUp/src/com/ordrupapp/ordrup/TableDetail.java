ce30c2303a39da02dca9e5b20c24d2acbc1f6f21
Charles Hafslund <charles.hafslund@gmail.com>
2013-07-10 09:36:48 -0500

    Added dynamic order buttons that use a  plate image as the background.  Still need to put in a scrollview for the table select and table details screen.

--- OrDrUp/src/com/ordrupapp/ordrup/TableDetail.java
+++ OrDrUp/src/com/ordrupapp/ordrup/TableDetail.java
@@ -2,14 +2,21 @@ package com.ordrupapp.ordrup;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class TableDetail extends Activity {
-
+	LinearLayout layout;
+	//need to make a call here to get orders for table
+	String orders[] = {"1","2"};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +31,7 @@ public class TableDetail extends Activity {
 		    TextView tableNumberText = (TextView) findViewById(R.id.table_details_number);
 		    String table = extras.getString("table");
 		    tableNumberText.setText(table);
+		    getOrders((View)findViewById(R.id.orderButtons_list));
 	
 		}
 		
@@ -45,6 +53,8 @@ public class TableDetail extends Activity {
 		getMenuInflater().inflate(R.menu.table_detail, menu);
 		return true;
 	}
+	
+	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -62,5 +72,65 @@ public class TableDetail extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	//dummy method, add real functionality
+public void addOrder(View view){
+	String[] newOrders = new String[orders.length +1];
+	for (int i = 0; i < orders.length; i++){
+		newOrders[i] = orders[i];
+	}
+	newOrders[orders.length] = "X";
+	orders = newOrders;
+	
+	getOrders((View)findViewById(R.id.orderButtons_list));
+}
+	
+public void getOrders(View view) {
+				
+		int orderCount = orders.length;
+		
+		sessionInfo mySession = ((sessionInfo)this.getApplication());
+		//debug message
+    	Toast.makeText(getApplicationContext(), "SiteCode: " + mySession.getSitecode() + "\nUsername: " + mySession.getUsername() + "\nPassword: " + mySession.getPasswordHash(), Toast.LENGTH_LONG).show();
+    	
+		Button btn[] = new Button[orderCount];
+		
+		if (null != layout && layout.getChildCount() > 0) {                 
+			try {
+				layout.removeViews (0, layout.getChildCount());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		//update this for orders versus tables
+		//Handler for dynamic buttons, passes in the table number
+		View.OnClickListener btnHandler = new View.OnClickListener() {
+		    public void onClick(View v) {
+		        Button orderButton = (Button)v;
+		        String buttonText = orderButton.getText().toString();
+		        //Intent intent = new Intent(v.getContext(), TableDetail.class);
+		        //intent.putExtra("table", buttonText);
+		        //startActivity(intent);
+		    }
+		};
+		
+		
+		layout = (LinearLayout) findViewById(R.id.orderButtons_list);
+		
+		//create the buttons based on the table list and assign the btnHandler to each
+		for (int i=0;i<orderCount;i++){
+			btn[i] = new Button(this);
+			btn[i].setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
+			btn[i].setBackgroundResource(R.drawable.large_plate);  // add image for kicks
+			btn[i].setText("Order " + orders[i]);
+			btn[i].setOnClickListener(btnHandler);
+
+			layout.addView(btn[i]);
+		}
+		
+	}
+	
 
 }
--- OrDrUp/src/com/ordrupapp/ordrup/TableSelect.java
+++ OrDrUp/src/com/ordrupapp/ordrup/TableSelect.java
@@ -19,6 +19,7 @@ public class TableSelect extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_table_select);
+		getTables((View)findViewById(R.id.tableButtons_list));
 	}
 
 	@Override
